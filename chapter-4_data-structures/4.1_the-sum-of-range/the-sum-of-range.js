450ab86b7806e4a64547b30ae6596161b43ec2f7
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-03-21 10:34:31 +0300

    the sum of range

---
+++ chapter-4_data-structures/4.1_the-sum-of-range/the-sum-of-range.js
@@ -0,0 +1,18 @@
+function range(start,end,step){
+    let array = [];
+    for(let i=start; i <= end;i+= step){
+    array += i;
+}
+return array;
+}
+console.log(range(4,12,2));
+
+
+function sum (...numbers){
+  let count = 0;
+  for(let n of numbers){
+   count += n ;
+  }
+  return count;
+}
+console.log(sum(2,3,4,5,6,7));
